d2283e39de09ee693b9e475636c69f5c2d58154f
jitendra khadoliya <jitendra.credentia@gmail.com>
2024-05-24 20:07:25 +0530

    New Updates: removed history

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,12 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import themeConfigSlice from './themeConfigSlice';
+import dataReducer from './dataSlice';
 
 const rootReducer = combineReducers({
     themeConfig: themeConfigSlice,
+    data: dataReducer,
 });
 
-export default configureStore({
+const store = configureStore({
     reducer: rootReducer,
 });
 
 export type IRootState = ReturnType<typeof rootReducer>;
+export default store;
