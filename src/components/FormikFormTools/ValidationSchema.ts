baa480793f73c0943079626de162401e3fc92c70
vicky654 <vickyverma2026@gmail.com>
2024-05-30 10:35:28 +0530

    add validation For add user

--- src/components/FormikFormTools/ValidationSchema.ts
+++ src/components/FormikFormTools/ValidationSchema.ts
@@ -2,17 +2,17 @@ import * as Yup from 'yup';
 
 export const validationSchema = Yup.object({
     first_name: Yup.string()
-    .min(8, 'First Name must be at least 8 characters')
+    .min(3, 'First Name must be at least 8 characters')
     .required('First Name Is Required'),
     last_name: Yup.string()
-    .min(8, 'Last Name be at least 8 characters')
+    .min(3, 'Last Name be at least 8 characters')
     .required('Last Name Is Required'),
     email: Yup.string()
     .required('Email Is Required'),
     password: Yup.string()
     .required('Password Is Required'),
-    phone_number: Yup.string()
-    .required('Phone Number Is Required'),
+    // phone_number: Yup.string()
+    // .required('Phone Number Is Required'),
     role: Yup.string()
     .required('Role Is Required'),
 });
