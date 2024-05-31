b8cca6b3830187e99dad429627c950ef617176c9
vicky654 <vickyverma2026@gmail.com>
2024-05-31 10:56:42 +0530

    token

--- src/utils/withApiHandler.tsx
+++ src/utils/withApiHandler.tsx
@@ -46,7 +46,7 @@ const withApiHandler = <P extends object>(WrappedComponent: React.ComponentType<
 
         axiosInstance.interceptors.request.use(
             (config) => {
-                const token = localStorage.getItem('tokens');
+                const token = localStorage.getItem('token');
                 if (token && config.headers) {
                     config.headers.Authorization = `Bearer ${token}`;
                 }
