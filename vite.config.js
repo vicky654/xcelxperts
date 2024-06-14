de6f54ab98757c84ca62870920e55746479ff726
jitendra khadoliya <jitendra.credentia@gmail.com>
2024-06-14 15:46:36 +0530

    add file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
@@ -16,4 +15,17 @@ export default defineConfig({
         port: 3003, // Replace 3000 with your desired port number
         host: '0.0.0.0',
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor'; // Split all node_modules into a separate chunk
+                    }
+                    // Add more conditions as needed for other large dependencies
+                }
+            }
+        },
+        chunkSizeWarningLimit: 1000, // Adjust this value as needed
+    }
 });
