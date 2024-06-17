9ed1ea2c91c976e8aaa311473136ab6f9ba86dd0
vicky654 <vickyverma2026@gmail.com>
2024-06-17 17:41:49 +0530

    Reset Item

--- src/pages/Authentication/RecoverIdCover.tsx
+++ src/pages/Authentication/RecoverIdCover.tsx
@@ -101,15 +101,15 @@ const RecoverIdCover = () => {
                     <img src="/assets/images/auth/coming-soon-object3.png" alt="image" className="absolute right-0 top-0 h-[300px]" />
                     <img src="/assets/images/auth/polygon-object.svg" alt="image" className="absolute bottom-0 end-[28%]" />
                     <div className="relative flex w-full max-w-[1502px] flex-col justify-between overflow-hidden rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 lg:min-h-[758px] lg:flex-row lg:gap-10 xl:gap-0">
-                        <div className="relative hidden w-full items-center justify-center bg-[linear-gradient(225deg,rgba(239,18,98,1)_0%,rgba(67,97,238,1)_100%)] p-5 lg:inline-flex lg:max-w-[835px] xl:-ms-28 ltr:xl:skew-x-[14deg] rtl:xl:skew-x-[-14deg]">
+                    <div  className="relative hidden w-full items-center justify-center bg-[linear-gradient(18deg,rgba(8,70,154,1)_0%,rgba(54,116,200,1)_62%,rgba(121,178,255,1)_100%)] p-5 lg:inline-flex lg:max-w-[835px] xl:-ms-28 ltr:xl:skew-x-[14deg] rtl:xl:skew-x-[-14deg]"><div className="absolute inset-y-0 w-8 from-primary/10 via-transparent to-transparent ltr:-right-10 ltr:bg-gradient-to-r rtl:-left-10 rtl:bg-gradient-to-l xl:w-16 ltr:xl:-right-20 rtl:xl:-left-20"></div>
                             <div className="absolute inset-y-0 w-8 from-primary/10 via-transparent to-transparent ltr:-right-10 ltr:bg-gradient-to-r rtl:-left-10 rtl:bg-gradient-to-l xl:w-16 ltr:xl:-right-20 rtl:xl:-left-20"></div>
                             <div className="ltr:xl:-skew-x-[14deg] rtl:xl:skew-x-[14deg]">
-                                <Link to="/" className="w-48 block lg:w-72 ms-10">
-                                    <img src="/assets/images/auth/logo-white.svg" alt="Logo" className="w-full" />
+                                <Link to="/" className="w-48 block lg:w-72 ms-10" style={{background:"#fff",borderRadius:"5px"}}>
+                                    <img src="/assets/images/xx_logo.png" alt="Logo" className="w-full" />
                                 </Link>
-                                <div className="mt-24 hidden w-full max-w-[430px] lg:block">
+                                {/* <div className="mt-24 hidden w-full max-w-[430px] lg:block">
                                     <img src="/assets/images/auth/reset-password.svg" alt="Cover Image" className="w-full" />
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                         <div className="relative flex w-full flex-col items-center justify-center gap-6 px-4 pb-16 pt-6 sm:px-6 lg:max-w-[667px]">
