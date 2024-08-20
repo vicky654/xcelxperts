9188406438bc4cf1f7e16d43271f098220b56382
vicky654 <vickyverma2026@gmail.com>
2024-08-20 14:40:23 +0530

    change Layout

--- src/components/DsrModules/DataEntryStatsComponent.tsx
+++ src/components/DsrModules/DataEntryStatsComponent.tsx
@@ -833,7 +833,7 @@ const DataEntryStatsComponent: React.FC<ManageSiteProps> = ({ postData, getData,
                 showStationInput={true}
                 showStationValidation={true}
                 validationSchema={validationSchemaForCustomInput}
-                layoutClasses="flex-1 grid grid-cols-1 sm:grid-cols-1 gap-5"
+                layoutClasses="flex-1 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4 xl:grid-cols-2 gap-5"
                 isOpen={false}
                 onClose={() => { }}
                 showDateInput={false}
