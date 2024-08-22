9ea4ba23de518b1438530acc1f3c2939c87e64b7
vicky654 <vickyverma2026@gmail.com>
2024-08-22 15:27:04 +0530

    fix issues In fuel ales

--- src/components/DsrModules/DataEntryStatsComponent.tsx
+++ src/components/DsrModules/DataEntryStatsComponent.tsx
@@ -599,28 +599,48 @@ const DataEntryStatsComponent: React.FC<ManageSiteProps> = ({ postData, getData,
                                   <p className="font-semibold w-1/6">Discount</p>
                                   <p className="font-semibold w-1/6">Net Value</p>
                                 </li>
-                                {subData?.map((fuelType, fuelIndex) =>
-                                  fuelType?.tanks?.map((tank: any, tankIndex: any) => (
-                                    <React.Fragment key={`${fuelIndex}-${tankIndex}`}>
+                                {subData?.map((fuelType, fuelIndex) => (
+                                  <div key={fuelIndex}>
+                                    <p className="p-2 bg-[#e5e7eb] font-bold w-1/10">Fuel Type: {fuelType?.name}</p>
+                                    {fuelType?.tanks?.map((tank: any, tankIndex: any) => (
+                                      <React.Fragment key={tankIndex}>
+                                        <p className="mt-2 mb-2 font-bold p-2 bg-[#e5e7eb] w-1/10">Tank Name: {tank?.tank_name}</p>
+                                        <div className="flex flex-col">
+                                          {tank?.nozzles?.map((nozzle: any, nozzleIndex: any) => (
+                                            <li key={nozzleIndex} className="flex justify-between p-2 hover:bg-gray-100 mt-2 " style={{
+                                              backgroundColor: nozzle?.id == "0" ? "#1c8b33a5" : "transparent",
+                                              fontWeight: nozzle?.id == "0" ? "bold" : ""
 
-                                      <p className=" p-2 bg-[#e5e7eb]  font-bold w-1/10"> Fuel Type: {fuelType?.name}</p>
-                                      <p className=" mt-2  mb-2 font-bold p-2 bg-[#e5e7eb] w-1/10"> Tank Name : {tank?.tank_name}</p>
-                                      <p className="w-4/6"></p> {/* Empty cells to fill the remaining space */}
+                                            }}>
+                                              <p className="w-1/6">{nozzle?.name} </p>
+                                              <p className="w-1/6">
+                                                {nozzle?.id == "0" ? `${nozzle?.price}` : `${currency} ${nozzle?.price}`}
+                                              </p>
+                                              <p className="w-1/6">{nozzle?.volume}</p>
+                                              <p className="w-1/6">{currency} {nozzle?.gross_value}</p>
+                                              <p className="w-1/6">{currency} {nozzle?.discount}</p>
+                                              <p className="w-1/6">{currency} {nozzle?.amount} </p>
+                                            </li>
+                                          ))}
 
-                                      {tank?.nozzles?.map((nozzle: any, nozzleIndex: any) => (
-                                        <li key={`${fuelIndex}-${tankIndex}-${nozzleIndex}`} className="flex justify-between p-2 hover:bg-gray-100">
+                                        </div>
 
-                                          <p className="w-1/6">{nozzle?.name}</p>
-                                          <p className="w-1/6">{currency} {nozzle?.price}</p>
-                                          <p className="w-1/6">{nozzle?.volume}</p>
-                                          <p className="w-1/6">{currency} {nozzle?.gross_value}</p>
-                                          <p className="w-1/6">{currency} {nozzle?.discount}</p>
-                                          <p className="w-1/6">{currency} {nozzle?.amount}</p>
-                                        </li>
-                                      ))}
-                                    </React.Fragment>
-                                  ))
-                                )}
+                                      </React.Fragment>
+
+                                    ))}
+                                    <li style={{ background: "#02449b47" }} key={fuelType} className="flex justify-between p-2 mt-2 mb-2 font-bold hover:bg-gray-100">
+                                      <p className="w-1/6 ">{fuelType?.name} Total</p>
+                                      <p className="w-1/6">
+
+                                      </p>
+                                      <p className="w-1/6">{fuelType?.volume}</p>
+                                      <p className="w-1/6">{currency} {fuelType?.gross_value}</p>
+                                      <p className="w-1/6">{currency} {fuelType?.discount}</p>
+                                      <p className="w-1/6">{currency} {fuelType?.amount} </p>
+                                    </li>
+                                  </div>
+                                ))}
+
                               </ul>
                             )}
                           </div>
@@ -692,21 +712,7 @@ const DataEntryStatsComponent: React.FC<ManageSiteProps> = ({ postData, getData,
                   </div>
                 )
               )}
-
-
-
-
-
             </div>
-
-
-
-
-
-
-
-
-
           </div>
         </div>
 
